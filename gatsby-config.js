bfe5081a398eec25d7970b0b38882ede903dee23
Kaleigh Scruggs <kaleigh.scruggs@northhighland.com>
2020-03-24 10:42:32 -0400

    fixed app name and updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Is It Snowing In Atlanta?`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
