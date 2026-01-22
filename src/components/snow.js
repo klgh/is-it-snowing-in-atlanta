f1ec47c2b1d321594abd95ef29322c79da36d576
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2026-01-22 18:30:56 -0500

    Update snow.js

--- src/components/snow.js
+++ src/components/snow.js
@@ -2,40 +2,40 @@ import React from "react"
 const Snow = () => {
   return (
     <div className="snow-container" aria-hidden="true">
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="snowflake"><div className="inner">❅</div></div>
       </div>
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="inner">❅</div>
       </div>
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="inner">❅</div>
       </div>
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="inner">❅</div>
       </div>
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="inner">❅</div>
       </div>
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="inner">❅</div>
       </div>
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="inner">❅</div>
       </div>
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="inner">❅</div>
       </div>
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="inner">❅</div>
       </div>
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="inner">❅</div>
       </div>
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="inner">❅</div>
       </div>
-      <div class="snowflake">
+      <div className="snowflake">
         <div className="inner">❅</div>
       </div>
     </div>
